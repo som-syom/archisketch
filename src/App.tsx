05bcdfc7e797565e0e44219d9489e5a572db84c5
som-syom <dhyeon@student.42seoul.kr>
2022-03-30 07:54:55 +0900

    ADD - redux 설치, 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './utils/styles/reset.css';
-import './App.css';
+import './utils/styles/common.css';
 import Header from './components/Header';
 import Gallery from './components/Gallery';
+import DetailView from './components/DetailView';
+import store from './store';
+import { Provider, useSelector } from 'react-redux';
+import { DetailViewState } from './store/reducers/detailView';
 
 const App: React.FC = () => {
+  // const isOpen = useSelector((state: DetailViewState) => state.isOpen);
+
   return (
-    <div className="App">
-      <Header />
-      <Gallery />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Gallery />
+        {/* {isOpen && <DetailView />} */}
+        <DetailView />
+      </div>
+    </Provider>
   );
 };
 
