e0f72c6a5c0129a81da7f41f70cdf5f237cf5db2
som-syom <dhyeon@student.42seoul.kr>
2022-03-30 04:43:04 +0900

    REMOVE - 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './utils/styles/reset.css';
 import './App.css';
 import Header from './components/Header';
-import { callAPI } from './api';
+import Gallery from './components/Gallery';
 
 const App: React.FC = () => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    const requestGET = async (): Promise<void> => {
-      await callAPI.get('/').then((res) => {
-        console.log(res.data);
-        setImages(res.data);
-      });
-    };
-    requestGET();
-  }, []);
-
   return (
     <div className="App">
-      {console.log(images)}
       <Header />
+      <Gallery />
     </div>
   );
 };
