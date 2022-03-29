827f6984d07d8f1e9e0dc4826d6ae9f7cc969970
som-syom <dhyeon@student.42seoul.kr>
2022-03-30 05:43:29 +0900

    ADD - mask 컴포넌트 분리

---
+++ src/components/ImageMask.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ImageMask: React.FC = () => {
+  return (
+    <Mask>
+      <CheckBox>
+        <CheckBoxInner>
+          <input type="checkbox" />
+        </CheckBoxInner>
+      </CheckBox>
+      <Menu type="button">
+        <MenuInner>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="64 64 896 896"
+            focusable="false"
+            data-icon="ellipsis"
+            width="1em"
+            height="1em"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M176 511a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0z" />
+          </svg>
+        </MenuInner>
+      </Menu>
+    </Mask>
+  );
+};
+
+const Mask = styled.div`
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  background-color: rgba(75, 79, 84, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // visibility: hidden;
+  opacity: 0;
+
+  &:hover {
+    // visibility: visible;
+    opacity: 1;
+  }
+`;
+
+const CheckBox = styled.label`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  line-height: 20px;
+`;
+
+const CheckBoxInner = styled.span`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  color: rgba(0, 0, 0, 0.65);
+  font-size: 14px;
+  position: relative;
+  top: -0.09em;
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  vertical-align: middle;
+  outline: none;
+  cursor: pointer;
+
+  & input {
+    // position: absolute;
+    // top: 0;
+    // left: 0;
+    // width: 100%;
+    // height: 100%;
+  }
+`;
+
+const Menu = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 15px;
+  color: rgb(255, 255, 255);
+  border: none;
+  background-color: transparent;
+  width: 32px;
+  height: 32px;
+  padding: 2.4px 0;
+  font-size: 16px;
+  border-radius: 2px;
+  line-height: 1.5715;
+  display: inline-block;
+  font-weight: 400;
+  white-space: nowrap;
+  text-align: center;
+  background-image: none;
+  box-shadow: 0 2px 0 rgb(0 0 0 / 2%);
+  cursor: pointer;
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  user-select: none;
+  touch-action: manipulation;
+`;
+
+const MenuInner = styled.span`
+  color: inherit;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+`;
+
+export default ImageMask;
